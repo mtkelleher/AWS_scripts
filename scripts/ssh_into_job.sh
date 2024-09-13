b0fb0fa884dc314f3e43c629db0110831fef1a4a
mtkelleher <126717928+mtkelleher@users.noreply.github.com>
2024-09-13 14:40:51 -0500

    Create ssh_into_job.sh

---
+++ scripts/ssh_into_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Note: In line 10 you must replace ip-head-node with your head node's ip!
+
+# Prompt the user for the job ID number
+echo "Enter the qstat job id number:"
+read input
+
+# Append .ip-head-node to the job id number
+input="${input}.ip-head-node"
+
+# Retrieve the compute node's IP address and SSH into it
+output=$(qstat -f "$input" | grep exec_host | awk '{print $3}' | sed 's/.*ip\.//' | cut -d'/' -f1)
+
+echo "Connecting via SSH to $output..."
+
+ssh "$output"
