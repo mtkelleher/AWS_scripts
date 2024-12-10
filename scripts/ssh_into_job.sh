95e5e0e5999310d52e8c75f4ff98395839e0c11a
mtkelleher <126717928+mtkelleher@users.noreply.github.com>
2024-12-10 15:21:03 -0600

    Update ssh_into_job.sh

--- scripts/ssh_into_job.sh
+++ scripts/ssh_into_job.sh
@@ -4,8 +4,11 @@
 echo "Enter the qstat job id number:"
 read input
 
-# Append .ip-{hostname -i} to the job id number
-input="${input}.ip-$(hostname -i)"
+# Extract the part before the first period from the hostname and prepend "ip-"
+host_ip=$(hostname | cut -d'.' -f1)
+
+# Append .{host_ip} to the job id number
+input="${input}.${host_ip}"
 
 # Retrieve the compute node's IP address and SSH into it
 output=$(qstat -f "$input" | grep exec_host | awk '{print $3}' | cut -d'/' -f1)
